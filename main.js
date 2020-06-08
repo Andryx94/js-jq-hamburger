e71b57b25286c5027344e421e06649ad1c49ef90
Andrea <andryx1994@gmail.com>
2020-06-08 15:28:15 +0200

    Versione 1.1
    Aggiunta versione "active"

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+// $(".fa-bars").click(
+//   function() {
+//     $(".hamburger-menu").show(200);
+//   }
+// );
+//
+// $(".fa-times").click(
+//   function() {
+//     $(".hamburger-menu").hide(200);
+//   }
+// );
+
 $(".fa-bars").click(
   function() {
-    $(".hamburger-menu").show(200);
+    $(".hamburger-menu").addClass("active");
   }
 );
 
 $(".fa-times").click(
   function() {
-    $(".hamburger-menu").hide(200);
+    $(".hamburger-menu").removeClass("active");
   }
 );
